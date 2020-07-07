0a311e936cf5cf6fc8b09bc4e91584453743d1cf
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-07-07 07:40:34 +0100

    S

--- CustomListView/app/src/main/java/com/dodemy/android/customlistview/CustomAdapter.java
+++ CustomListView/app/src/main/java/com/dodemy/android/customlistview/CustomAdapter.java
@@ -15,57 +15,28 @@ import com.google.android.material.snackbar.Snackbar;
 import java.util.ArrayList;
 
 
-public class CustomAdapter extends ArrayAdapter<DataModel> implements View.OnClickListener{
-
-    private ArrayList<DataModel> dataSet;
+public class CustomAdapter extends ArrayAdapter<DataModel> implements View.OnClickListener {
     Context mContext;
-
-    // View lookup cache
-    private static class ViewHolder {
-        TextView txtName;
-        TextView txtType;
-        TextView txtVersion;
-        ImageView info;
-    }
-
-
+    private ArrayList<DataModel> dataSet;
+    private int lastPosition = -1;
 
     public CustomAdapter(ArrayList<DataModel> data, Context context) {
         super(context, R.layout.row_item, data);
         this.dataSet = data;
-        this.mContext=context;
-
+        this.mContext = context;
     }
-
 
     @Override
     public void onClick(View v) {
-
-
-        int position=(Integer) v.getTag();
-        Object object= getItem(position);
-        DataModel dataModel=(DataModel)object;
-
-
-
-
-        switch (v.getId())
-        {
-
-            case R.id.item_info:
-
-                Snackbar.make(v, "Release date " +dataModel.getFeature(), Snackbar.LENGTH_LONG)
-                        .setAction("No action", null).show();
-
-                break;
-
-
+        int position = (Integer) v.getTag();
+        Object object = getItem(position);
+        DataModel dataModel = (DataModel) object;
+        if (v.getId() == R.id.item_info) {
+            assert dataModel != null;
+            Snackbar.make(v, "Release date " + dataModel.getFeature(), Snackbar.LENGTH_LONG)
+                    .setAction("No action", null).show();
         }
-
-
     }
-
-    private int lastPosition = -1;
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -73,12 +44,8 @@ public class CustomAdapter extends ArrayAdapter<DataModel> implements View.OnCli
         DataModel dataModel = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         ViewHolder viewHolder; // view lookup cache stored in tag
-
         final View result;
-
         if (convertView == null) {
-
-
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.row_item, parent, false);
@@ -86,20 +53,17 @@ public class CustomAdapter extends ArrayAdapter<DataModel> implements View.OnCli
             viewHolder.txtType = (TextView) convertView.findViewById(R.id.type);
             viewHolder.txtVersion = (TextView) convertView.findViewById(R.id.version_number);
             viewHolder.info = (ImageView) convertView.findViewById(R.id.item_info);
-
-            result=convertView;
-
+            result = convertView;
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
-            result=convertView;
+            result = convertView;
         }
-
         Animation animation = AnimationUtils.loadAnimation(mContext, (position > lastPosition) ? R.anim.up_from_bottom : R.anim.down_from_top);
         result.startAnimation(animation);
         lastPosition = position;
 
-
+        assert dataModel != null;
         viewHolder.txtName.setText(dataModel.getName());
         viewHolder.txtType.setText(dataModel.getType());
         viewHolder.txtVersion.setText(dataModel.getVersion_number());
@@ -109,5 +73,11 @@ public class CustomAdapter extends ArrayAdapter<DataModel> implements View.OnCli
         return convertView;
     }
 
-
+    // View lookup cache
+    private static class ViewHolder {
+        TextView txtName;
+        TextView txtType;
+        TextView txtVersion;
+        ImageView info;
+    }
 }
